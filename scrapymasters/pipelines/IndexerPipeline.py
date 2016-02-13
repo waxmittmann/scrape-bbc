fbab4be03648aa5376db4cb4a40861754a63dbe3
Max Wittman <Max.Wittman@redballoon.com.au>
2016-02-14 00:44:42 +1100

    Cleanup

--- scrapymasters/pipelines/IndexerPipeline.py
+++ scrapymasters/pipelines/IndexerPipeline.py
@@ -1,7 +1,5 @@
 from string import punctuation
 from scrapymasters.processing.OutputWriter import OutputWriter
-import urllib
-import cgi
 
 
 class IndexerPipeline(object):
@@ -14,21 +12,7 @@ class IndexerPipeline(object):
     def process_item(self, article, spider):
         body = article['body']
         for word in body.split():
-            # word = urllib.urlencode(word.strip(punctuation).lower())
-            # word = cgi.escape(word.strip(punctuation).lower())
-            # word = urllib.quote(word.strip(punctuation).lower())
-
-            #remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-            #word_list = [s.translate(remove_punctuation_map) for s in value_list]
-
-            # oldWord = word
             word = word.strip(punctuation).lower()
-            # word = word.translate(None, '.')
-            # word = [word.translate(self.remove_punctuation_map) for s in value_list]
-
-            # print(oldWord)
-            # if oldWord == "71.8%":
-            #     print("Conversion: " + word + " = " + oldWord)
 
             if len(word) != 0 and word.isalpha():
                 url = article['url']
@@ -45,16 +29,9 @@ class IndexerPipeline(object):
     # as an exporter or something seems harder. Maybe a better alternative would be to add a separate 'final' pipeline
     # stage that does the article-storing, processing and writing.
     def close_spider(self, spider):
-        just_articles = {
-            'articles': self.articles
-        }
-
         articles_and_index = {
             'articles': self.articles,
             'index': self.article_word_index
         }
         self.outputWriter.write_to_file(articles_and_index)
         self.outputWriter.write_to_mongo(articles_and_index)
-        # print("type of articles: ")
-        # print(type(self.articles))
-        # self.outputWriter.write_to_mongo(just_articles)
--- scrapymasters/processing/OutputWriter.py
+++ scrapymasters/processing/OutputWriter.py
@@ -7,7 +7,6 @@ class OutputWriter:
     def __init__(self):
         config = ConfigParser.ConfigParser()
         config.read("config.ini")
-        # config.options("mongo")
 
         self.url = config.get("Mongo", "url")
         self.username = config.get("Mongo", "username")
@@ -21,7 +20,6 @@ class OutputWriter:
 
     def write_to_mongo(self, data):
         print("Beginning mongo write")
-        # mongodb://<username>:<password>@aws-us-east-1-portal.9.dblayer.com:10650,aws-us-east-1-portal.4.dblayer.com:11181/admin
         client = MongoClient("mongodb://" + self.username + ":" + self.password + "@" + self.url + "/" + self.dbname)
         db = client.scrape
         db.articles.insert_one(data)
