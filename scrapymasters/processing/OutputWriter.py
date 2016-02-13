11dfe4981e7e69b5a9ccb0f71fda050afa7bfa42
Max Wittman <Max.Wittman@redballoon.com.au>
2016-02-14 00:07:40 +1100

    Make a start on writing to mongo

--- scrapymasters/processing/OutputWriter.py
+++ scrapymasters/processing/OutputWriter.py
@@ -1,9 +1,20 @@
 import json
-
+from pymongo import MongoClient
+import ConfigParser
 
 class OutputWriter:
+    url = None
+    username = None
+    password = None
+
     def __init__(self):
-        pass
+        config = ConfigParser.ConfigParser()
+        config.read("config.ini")
+        config.options("mongo")
+
+        self.url = config.get("Mongo", "url")
+        self.username = config.get("Mongo", "username")
+        self.password = config.get("Mongo", "password")
 
     @staticmethod
     def write_to_file(data):
@@ -11,6 +22,8 @@ class OutputWriter:
             json.dump(data, fp)
 
     @staticmethod
-    def write_to_mongo(data):
-        with open('crawler-output.json', 'w') as fp:
-            json.dump(data, fp)
+    def write_to_mongo(self, data):
+
+        # mongodb://<username>:<password>@aws-us-east-1-portal.9.dblayer.com:10650,aws-us-east-1-portal.4.dblayer.com:11181/admin
+        client = MongoClient("mongodb://" + self.username + ":" + self.password + "@" + self.url + "/admin")
+
