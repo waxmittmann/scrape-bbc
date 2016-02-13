a92bf562141118fb4f0e6a8015d21e7459fa9763
Max Wittman <Max.Wittman@redballoon.com.au>
2016-02-13 17:37:39 +1100

    Forgetting 'return' == basd
    Former-commit-id: 89000764d6c52bd85f676f3d3d71ff4414c9f943

--- scrapymasters/pipelines/IndexerPipeline.py
+++ scrapymasters/pipelines/IndexerPipeline.py
@@ -9,8 +9,21 @@ class IndexerPipeline(object):
         self.ids_seen = set()
 
     def process_item(self, item, spider):
+        print("Got: " + str(item))
+        return item
+        # print(item)
         # with open('guardian-articles.json') as data_file:
         # articles = json.load(data_file)
-        for article in item:
-            print("Processing ")
-            print(article)
+
+        # for article in item:
+        #     print("Processing: ")
+        #     print(article)
+        #
+        #     if type(article) is list:
+        #         for i in article:
+        #             yield i
+        #     else:
+        #         yield article
+            # print("Processing ")
+            # print(article)
+            # yield article
