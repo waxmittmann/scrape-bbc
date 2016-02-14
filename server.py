e4cd29a433b43fed4d52f59927b1714b183989a5
Max Wittman <Max.Wittman@redballoon.com.au>
2016-02-14 19:26:43 +1100

    Remove attempt to run spider from server; remove setup.py for packaging project.

--- server.py
+++ server.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 import web
-import scrapy
-from scrapy.crawler import CrawlerProcess
-from scrapy.crawler import CrawlerRunner
-from twisted.internet import reactor
-from scrapymasters.spiders.BBCSpider import BBCSpider
 from scrapymasters.common.ConfigFiles import ConfigFiles
 from scrapymasters.common.MongoUtils import MongoUtils
 
@@ -25,24 +20,9 @@ class Articles:
         client = MongoUtils.create_client_from_config(self.config)
         db = client.scrape
         articles = MongoUtils.find_all_articles(db)
-        # articles = db.articles.find()
         client.close()
         return articles
 
-    def PUT(self):
-        runner = CrawlerRunner()
-        runner.crawl(BBCSpider)
-        d = runner.join()
-        # d.addBoth(lambda _: reactor.stop())
-
-        reactor.run(installSignalHandlers=0) # the script will block
-
-        # process = CrawlerProcess({
-        #     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-        # })
-        #
-        # process.crawl(BBCSpider)
-        # process.start() # the script will block here until the crawling is finished
 
 class WordsGetOne:
     def __init__(self):
