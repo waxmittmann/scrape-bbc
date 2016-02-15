9c969c129e7ac5624272d41ad69d366a9452952d
Max Wittman <Max.Wittman@redballoon.com.au>
2016-02-15 20:04:57 +1100

    Clean up and add shell script for running tests

--- scrapymasters/test/TestSuite.py
+++ scrapymasters/test/TestSuite.py
@@ -1,13 +1,4 @@
-# I'm sure that this is not good practice; I've come across using setuptools / setup.py to do a local dev install
-# (e.g. http://stackoverflow.com/questions/1893598/pythonpath-vs-sys-path), but I just don't have the time to figure
-# out the intricacies of that mechanism, so proper deployment will have to fall to the wayside for now.
-import sys
-import os
 import unittest
-
-sys.path.insert(1, os.getcwd())
-from scrapymasters.test.StringUtilTest import StringUtilTest
-from scrapymasters.test.XpathUtilTest import XpathUtilTest
 
 
 class TestSuite(unittest.TestCase):
