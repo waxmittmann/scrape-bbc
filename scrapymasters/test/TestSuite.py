97c448a48cf5ef90df49f062f22d64a0244905e0
Max Wittman <Max.Wittman@redballoon.com.au>
2016-02-15 20:06:59 +1100

    cleanup

--- scrapymasters/test/TestSuite.py
+++ scrapymasters/test/TestSuite.py
@@ -3,11 +3,5 @@ import unittest
 
 class TestSuite(unittest.TestCase):
     if __name__ == '__main__':
-        # suite = unittest.TestLoader()\
-        #     .loadTestsFromTestCase(StringUtilTest)\
-        #     .loadTestsFromTestCase(XpathUtilTest)
-
-        # suite = unittest.TestLoader()._find_tests(".", "*Test.py").
-        # suite = unittest.TestLoader().loadTestsFromModule("scrapymasters.test")
         suite = unittest.TestLoader().discover(".", pattern='*Test.py', top_level_dir=None)
         unittest.TextTestRunner(verbosity=2).run(suite)
